7e9d6384bd4df13e310c075830af7125f2f95537
Logan Gore <gorel@purdue.edu>
2016-03-01 13:47:37 -0500

    Switch to xrange

--- census/census.py
+++ census/census.py
@@ -3,9 +3,9 @@ import sys
 N = int(sys.stdin.readline())
 ages = [0 for i in range(100)]
 
-for i in range(N):
+for i in xrange(N):
     citizen = int(sys.stdin.readline())
     ages[citizen] += 1
 
-for i in range(1, 100):
+for i in xrange(1, 100):
     print(ages[i])
--- census/gen.py
+++ census/gen.py
@@ -1,8 +1,8 @@
 import random
 
-# 25 million
-N = 25 * 1000 * 1000
+# 10 million
+N = 10 * 1000 * 1000
 
 print(N)
-for i in range(N):
+for i in xrange(N):
     print(random.randint(1, 99))
